9262f0c63ab0b821ae0144955d1198d9739db43f
mozhinag <manimozhinagooran06@gmail.com>
2025-09-19 22:00:46 +0530

    error

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
-const btn = document.getElementById('menu-btn')
-const nav = document.getElementById('menu')
+document.addEventListener('DOMContentLoaded', () => {
+  const btn = document.getElementById('menu-btn')
+  const nav = document.getElementById('menu')
 
-btn.addEventListener('click', () => {
-  btn.classList.toggle('open')
-  nav.classList.toggle('flex')
-  nav.classList.toggle('hidden')
+  btn.addEventListener('click', () => {
+    btn.classList.toggle('open')
+    nav.classList.toggle('flex')
+    nav.classList.toggle('hidden')
+  })
+
+
 })
+ 
